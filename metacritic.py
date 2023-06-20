e5864c7af6cbb44780f1fb2d5c0a0d57b7f1d8ef
neutrogo <erbal98@gmail.com>
2023-06-20 14:43:53 +0100

    added methods to get metacritic aggregate scores

--- metacritic.py
+++ metacritic.py
@@ -30,15 +30,31 @@ def get_available_plats(res_list, user_input, user_agent):
 
 def get_user_score(game, plat, user_agent):
     """gets the user review score for the given game & platform"""
-    game = game.replace(" ", "-").lower()
-    plat = plat.replace(" ", "-").lower()
+    game, plat = convert_name_to_path(game, plat)
+    soup = get_soup(user_agent, game, plat)
+    user_score = soup.select_one("div.metascore_w.user").text
+    return user_score
+
+def get_critic_score(game, plat, user_agent):
+    """gets the critic review score for the given game & platform"""
+    game, plat = convert_name_to_path(game, plat)
+    soup = get_soup(user_agent, game, plat)
+    critic_score = soup.select_one("div.metascore_w.xlarge").text
+    return critic_score
+
+def get_soup(user_agent, game, plat):
+    """creates and returns a BeautifulSoup object for the given url"""
     url = f"https://www.metacritic.com/game/{plat}/{game}"
     page = requests.get(url, headers=user_agent)
     soup = BeautifulSoup(page.content, 'html.parser')
-    res = soup.select_one("div.metascore_w.user")
-    user_score = res.text
-    return user_score
+    return soup
 
+def convert_name_to_path(game, plat):
+    """Turns game and platform names to useable metacritic paths"""
+    game = game.replace(" ", "-").lower()
+    plat = plat.replace(" ", "-").lower()
+    return (game, plat)
+    
 url = "https://www.metacritic.com/search/game/GAMENAME/results"
 user_agent = {'User-agent': 'Mozilla/5.0'}
 
@@ -53,4 +69,8 @@ for game in res:
     res_list.append(game)
 
 plats = get_available_plats(res_list, "Elden Ring", user_agent)
-score = get_user_score("Elden Ring", "Playstation 5", user_agent)
+user = get_user_score("Elden Ring", "Playstation 5", user_agent)
+critic = get_critic_score("Elden Ring", "Playstation 5", user_agent)
+
+print(user)
+print(critic)
